971e2c8390742154083f6b2f815ce10154eb9629
cmaria08 <cmaria@plazalama.com>
2025-02-25 23:52:38 -0400

    Remove unnecessary AppController, AppService, and example DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerModule, EnvModule, DatabaseModule } from './config';
 import { HealthModule, ProductModule } from './modules';
 
@@ -12,7 +10,5 @@ import { HealthModule, ProductModule } from './modules';
     ProductModule,
     HealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
